857ccf2b9e583d5909b77541656bea381990258d
Elise <60504990+Passiflore@users.noreply.github.com>
2025-12-01 22:20:28 +0100

    FEAT(header): Replace App component styles with SCSS, integrate new Header component, and update main entry point to use SCSS.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <p className={styles.title}>Green Buddy 🌿</p>
+    </header>
+  );
+};
+
+export default Header;
